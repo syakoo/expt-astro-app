78ec20853e7a6db117b300f4c690781f7c7e8d87
Takuya Sakoda (syakoo) <syakoo.pub@gmail.com>
2023-08-18 16:10:14 +0900

    ✨ feat: update settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   base: import.meta.env.PROD ? "/app-root" : "",
+  compressHTML: true,
   server: {
     host: true,
   },
