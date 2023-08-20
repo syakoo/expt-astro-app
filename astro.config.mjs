fa52f31eb497df2a577f52059e41f203adf926dc
Takuya Sakoda (syakoo) <syakoo.pub@gmail.com>
2023-08-21 08:54:53 +0900

    ✨ feat: update project base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 import { defineConfig } from "astro/config";
 
+const BUCKET_BASE = "/app-root";
+const PROJECT_BASE = "/projects/posts/20230820";
+
 // https://astro.build/config
 export default defineConfig({
-  base: import.meta.env.PROD ? "/app-root" : "",
+  srcDir: `.${PROJECT_BASE}`,
+  base: import.meta.env.PROD ? `.${BUCKET_BASE}${PROJECT_BASE}` : "",
+  outDir: import.meta.env.PROD ? `./dist${BUCKET_BASE}${PROJECT_BASE}` : "",
   compressHTML: true,
   server: {
     host: true,
+  },
+  build: {
+    assets: "_assets",
   },
   experimental: {
     assets: true,
